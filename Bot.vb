c517a571e813563834777514841cb32804c09759
thesuperdanb <thesuperdanb@chaosterminal>
2014-07-08 18:55:59 -0500

    Undo Load_Bot.vb Module

--- Bot.vb
+++ Bot.vb
@@ -4,7 +4,34 @@ Public Class Bot
     Dim last_tab As System.Windows.Forms.TabPage
     Private Sub Bot_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         TabControl.SelectedTab = Home
-        Call Load_Bot_Sub()
+        Dim strFileSize As String = ""
+        Dim di As New IO.DirectoryInfo(My.Application.Info.DirectoryPath)
+        Dim aryFi As IO.FileInfo() = di.GetFiles("*.account")
+        Dim fi As IO.FileInfo
+        For Each fi In aryFi
+            Dim Noconfig As String = Replace(fi.Name, ".account", "")
+            Dim URL As String
+
+            Dim reader As New System.IO.StreamReader(My.Application.Info.DirectoryPath & "/" & Noconfig & ".account")
+
+            URL = reader.ReadLine()
+
+            reader.Close()
+
+            Dim tabpage As New TabPage
+
+            tabpage.Text = Noconfig
+
+            Dim WebBrowser As New WebBrowser
+
+            WebBrowser.Parent = tabpage
+
+            WebBrowser.Navigate(URL)
+            WebBrowser.Location = New Point(10, 10)
+            WebBrowser.Dock = DockStyle.Fill
+
+            TabControl.TabPages.Add(tabpage)
+        Next
     End Sub
 
     Private Sub Add_Click(sender As Object, e As EventArgs) Handles Add.Click
