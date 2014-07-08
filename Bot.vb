380636fb04a05f71d696a693166b53b3bd19d7e7
thesuperdanb <thesuperdanb@chaosterminal.com>
2014-07-08 16:41:55 -0500

    Load Module Added

--- Bot.vb
+++ Bot.vb
@@ -4,41 +4,14 @@ Public Class Bot
     Dim last_tab As System.Windows.Forms.TabPage
     Private Sub Bot_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         TabControl.SelectedTab = Home
-        Dim strFileSize As String = ""
-        Dim di As New IO.DirectoryInfo(My.Application.Info.DirectoryPath)
-        Dim aryFi As IO.FileInfo() = di.GetFiles("*.account")
-        Dim fi As IO.FileInfo
-        For Each fi In aryFi
-            Dim Noconfig As String = Replace(fi.Name, ".account", "")
-            Dim URL As String
-
-            Dim reader As New System.IO.StreamReader(My.Application.Info.DirectoryPath & "/" & Noconfig & ".account")
-
-            URL = reader.ReadLine()
-
-            reader.Close()
-
-            Dim tabpage As New TabPage
-
-            tabpage.Text = Noconfig
-
-            Dim WebBrowser As New WebBrowser
-
-            WebBrowser.Parent = tabpage
-
-            WebBrowser.Navigate(URL)
-            WebBrowser.Location = New Point(10, 10)
-            WebBrowser.Dock = DockStyle.Fill
-
-            TabControl.TabPages.Add(tabpage)
-        Next
+        Call Load_Bot_Sub()
     End Sub
 
     Private Sub Add_Click(sender As Object, e As EventArgs) Handles Add.Click
         FileSystem.WriteAllText(My.Application.Info.DirectoryPath & "/" & Username.Text & ".account", Link.Text, True)
         Dim Restart As Integer = MessageBox.Show("Bot Must Reload", "Refresh", MessageBoxButtons.OKCancel)
         If Restart = DialogResult.OK Then
-            Application.Restart()
+            Call Load_Bot_Sub()
         End If
     End Sub
 
