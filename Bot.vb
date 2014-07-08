1598bf7fb4dd6505baece08f6fe909d737c765b0
thesuperdanb <thesuperdanb@chaosterminal.com>
2014-07-08 16:37:49 -0500

    Removed My.Computer.FileSystem

--- Bot.vb
+++ Bot.vb
@@ -1,4 +1,6 @@
-﻿Public Class Bot
+﻿Imports Microsoft.VisualBasic.FileIO
+
+Public Class Bot
     Dim last_tab As System.Windows.Forms.TabPage
     Private Sub Bot_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         TabControl.SelectedTab = Home
@@ -33,7 +35,7 @@
     End Sub
 
     Private Sub Add_Click(sender As Object, e As EventArgs) Handles Add.Click
-        My.Computer.FileSystem.WriteAllText(My.Application.Info.DirectoryPath & "/" & Username.Text & ".account", Link.Text, True)
+        FileSystem.WriteAllText(My.Application.Info.DirectoryPath & "/" & Username.Text & ".account", Link.Text, True)
         Dim Restart As Integer = MessageBox.Show("Bot Must Reload", "Refresh", MessageBoxButtons.OKCancel)
         If Restart = DialogResult.OK Then
             Application.Restart()
