187f68b438cf91daddbc9074a3732b5a708e9419
thesuperdanb <thesuperdanb@chaosterminal>
2014-07-08 19:06:22 -0500

    Multiple Proxy Script

--- Bot.vb
+++ Bot.vb
@@ -1,6 +1,35 @@
 ﻿Imports Microsoft.VisualBasic.FileIO
+Imports System.Runtime.InteropServices
 
 Public Class Bot
+#Region "Using Proxy"
+    <Runtime.InteropServices.DllImport("wininet.dll", SetLastError:=True)> _
+    Private Shared Function InternetSetOption(ByVal hInternet As IntPtr, ByVal dwOption As Integer, ByVal lpBuffer As IntPtr, ByVal lpdwBufferLength As Integer) As Boolean
+    End Function
+
+    Public Structure Struct_INTERNET_PROXY_INFO
+        Public dwAccessType As Integer
+        Public proxy As IntPtr
+        Public proxyBypass As IntPtr
+    End Structure
+
+    Private Sub UseProxy(ByVal strProxy As String)
+        Const INTERNET_OPTION_PROXY As Integer = 38
+        Const INTERNET_OPEN_TYPE_PROXY As Integer = 3
+
+        Dim struct_IPI As Struct_INTERNET_PROXY_INFO
+
+        struct_IPI.dwAccessType = INTERNET_OPEN_TYPE_PROXY
+        struct_IPI.proxy = Marshal.StringToHGlobalAnsi(strProxy)
+        struct_IPI.proxyBypass = Marshal.StringToHGlobalAnsi("local")
+
+        Dim intptrStruct As IntPtr = Marshal.AllocCoTaskMem(Marshal.SizeOf(struct_IPI))
+
+        Marshal.StructureToPtr(struct_IPI, intptrStruct, True)
+
+        Dim iReturn As Boolean = InternetSetOption(IntPtr.Zero, INTERNET_OPTION_PROXY, intptrStruct, System.Runtime.InteropServices.Marshal.SizeOf(struct_IPI))
+    End Sub
+#End Region
     Dim last_tab As System.Windows.Forms.TabPage
     Private Sub Bot_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         TabControl.SelectedTab = Home
