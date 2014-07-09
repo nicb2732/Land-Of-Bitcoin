2932400f7ba8e1269957c739ac474e2db12e6874
thesuperdanb <thesuperdanb@chaosterminal.com>
2014-07-09 15:49:17 -0500

    Icon Added

--- Bot.Designer.vb
+++ Bot.Designer.vb
@@ -22,22 +22,23 @@ Partial Class Bot
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Bot))
         Me.TabControl = New System.Windows.Forms.TabControl()
+        Me.Accounts = New System.Windows.Forms.TabPage()
+        Me.FileBrowser = New System.Windows.Forms.WebBrowser()
         Me.Home = New System.Windows.Forms.TabPage()
+        Me.Change_Folder = New System.Windows.Forms.Button()
+        Me.Folder_Label = New System.Windows.Forms.Label()
+        Me.Folder = New System.Windows.Forms.TextBox()
         Me.Add = New System.Windows.Forms.Button()
         Me.Proxy_Link_Label = New System.Windows.Forms.Label()
         Me.Link = New System.Windows.Forms.TextBox()
         Me.Username_Label = New System.Windows.Forms.Label()
         Me.Username = New System.Windows.Forms.TextBox()
         Me.Bot_designed_by_Chaos_Terminal = New System.Windows.Forms.Label()
-        Me.Accounts = New System.Windows.Forms.TabPage()
-        Me.FileBrowser = New System.Windows.Forms.WebBrowser()
-        Me.Folder = New System.Windows.Forms.TextBox()
-        Me.Folder_Label = New System.Windows.Forms.Label()
-        Me.Change_Folder = New System.Windows.Forms.Button()
         Me.TabControl.SuspendLayout()
-        Me.Home.SuspendLayout()
         Me.Accounts.SuspendLayout()
+        Me.Home.SuspendLayout()
         Me.SuspendLayout()
         '
         'TabControl
@@ -52,6 +53,25 @@ Partial Class Bot
         Me.TabControl.SelectedIndex = 0
         Me.TabControl.Size = New System.Drawing.Size(705, 510)
         Me.TabControl.TabIndex = 2
+        '
+        'Accounts
+        '
+        Me.Accounts.Controls.Add(Me.FileBrowser)
+        Me.Accounts.Location = New System.Drawing.Point(4, 33)
+        Me.Accounts.Name = "Accounts"
+        Me.Accounts.Size = New System.Drawing.Size(697, 473)
+        Me.Accounts.TabIndex = 1
+        Me.Accounts.Text = "Accounts"
+        Me.Accounts.UseVisualStyleBackColor = True
+        '
+        'FileBrowser
+        '
+        Me.FileBrowser.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.FileBrowser.Location = New System.Drawing.Point(0, 0)
+        Me.FileBrowser.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.FileBrowser.Name = "FileBrowser"
+        Me.FileBrowser.Size = New System.Drawing.Size(697, 473)
+        Me.FileBrowser.TabIndex = 0
         '
         'Home
         '
@@ -73,6 +93,36 @@ Partial Class Bot
         Me.Home.TabIndex = 0
         Me.Home.Text = "Home"
         Me.Home.UseVisualStyleBackColor = True
+        '
+        'Change_Folder
+        '
+        Me.Change_Folder.Location = New System.Drawing.Point(7, 298)
+        Me.Change_Folder.Margin = New System.Windows.Forms.Padding(2)
+        Me.Change_Folder.Name = "Change_Folder"
+        Me.Change_Folder.Size = New System.Drawing.Size(683, 45)
+        Me.Change_Folder.TabIndex = 8
+        Me.Change_Folder.Text = "Change Folder"
+        Me.Change_Folder.UseVisualStyleBackColor = True
+        '
+        'Folder_Label
+        '
+        Me.Folder_Label.AutoSize = True
+        Me.Folder_Label.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Folder_Label.Location = New System.Drawing.Point(18, 242)
+        Me.Folder_Label.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Folder_Label.Name = "Folder_Label"
+        Me.Folder_Label.Size = New System.Drawing.Size(149, 24)
+        Me.Folder_Label.TabIndex = 7
+        Me.Folder_Label.Text = "Accounts Folder"
+        '
+        'Folder
+        '
+        Me.Folder.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Folder.Location = New System.Drawing.Point(7, 268)
+        Me.Folder.Margin = New System.Windows.Forms.Padding(2)
+        Me.Folder.Name = "Folder"
+        Me.Folder.Size = New System.Drawing.Size(683, 26)
+        Me.Folder.TabIndex = 6
         '
         'Add
         '
@@ -136,69 +186,21 @@ Partial Class Bot
         Me.Bot_designed_by_Chaos_Terminal.TabIndex = 0
         Me.Bot_designed_by_Chaos_Terminal.Text = "Bot designed by Chaos Terminal" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
         '
-        'Accounts
-        '
-        Me.Accounts.Controls.Add(Me.FileBrowser)
-        Me.Accounts.Location = New System.Drawing.Point(4, 33)
-        Me.Accounts.Name = "Accounts"
-        Me.Accounts.Size = New System.Drawing.Size(697, 350)
-        Me.Accounts.TabIndex = 1
-        Me.Accounts.Text = "Accounts"
-        Me.Accounts.UseVisualStyleBackColor = True
-        '
-        'FileBrowser
-        '
-        Me.FileBrowser.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.FileBrowser.Location = New System.Drawing.Point(0, 0)
-        Me.FileBrowser.MinimumSize = New System.Drawing.Size(20, 20)
-        Me.FileBrowser.Name = "FileBrowser"
-        Me.FileBrowser.Size = New System.Drawing.Size(697, 350)
-        Me.FileBrowser.TabIndex = 0
-        '
-        'Folder
-        '
-        Me.Folder.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Folder.Location = New System.Drawing.Point(7, 268)
-        Me.Folder.Margin = New System.Windows.Forms.Padding(2)
-        Me.Folder.Name = "Folder"
-        Me.Folder.Size = New System.Drawing.Size(683, 26)
-        Me.Folder.TabIndex = 6
-        '
-        'Folder_Label
-        '
-        Me.Folder_Label.AutoSize = True
-        Me.Folder_Label.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Folder_Label.Location = New System.Drawing.Point(18, 242)
-        Me.Folder_Label.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
-        Me.Folder_Label.Name = "Folder_Label"
-        Me.Folder_Label.Size = New System.Drawing.Size(149, 24)
-        Me.Folder_Label.TabIndex = 7
-        Me.Folder_Label.Text = "Accounts Folder"
-        '
-        'Change_Folder
-        '
-        Me.Change_Folder.Location = New System.Drawing.Point(7, 298)
-        Me.Change_Folder.Margin = New System.Windows.Forms.Padding(2)
-        Me.Change_Folder.Name = "Change_Folder"
-        Me.Change_Folder.Size = New System.Drawing.Size(683, 45)
-        Me.Change_Folder.TabIndex = 8
-        Me.Change_Folder.Text = "Change Folder"
-        Me.Change_Folder.UseVisualStyleBackColor = True
-        '
         'Bot
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(705, 510)
         Me.Controls.Add(Me.TabControl)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Margin = New System.Windows.Forms.Padding(2)
         Me.Name = "Bot"
         Me.ShowIcon = False
         Me.Text = "Land of Bitcoin Faucet Bot"
         Me.TabControl.ResumeLayout(False)
+        Me.Accounts.ResumeLayout(False)
         Me.Home.ResumeLayout(False)
         Me.Home.PerformLayout()
-        Me.Accounts.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
